606df4888f82204fd514e2de849929eb7002eaef
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-05-05 12:33:22 +0530

    Change title from 'Developer' to 'Frontend Developer'

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <h1 className="text-5xl md:text-6xl font-extrabold mb-4 leading-tight">
         Hi, I'm{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
-          Developer
+         Frontend Developer
         </span>{" "}
         👋
       </h1>
